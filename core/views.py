567c83bd913a8752f4ca99eaf4453086cc9aa56d
kibet <kibet@kibet.edu>
2025-03-03 12:51:11 +0300

    social book as a new personal project

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        passsword2 = request.POST['password2']
+        
+    else:
+        return render(request, 'signup.html')
