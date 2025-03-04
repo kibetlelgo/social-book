1911e5041661d9cd19718fff4593f8bc573d1b8b
kibet <kibetlelgo49@gmail.com>
2025-03-04 08:17:23 +0300

    set up signup and sigh in authentication
    new commit

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 def index(request):
     return render(request, 'index.html')
@@ -12,5 +15,45 @@ def signup(request):
         password = request.POST['password']
         passsword2 = request.POST['password2']
         
+        if password == passsword2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')    
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+
+                #create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+
+
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+        
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
