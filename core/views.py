3790ea548ff5c308ed2c1adf841a916f3d45b5a9
kibet <kibetlelgo49@gmail.com>
2025-03-17 19:17:45 +0300

    Like and unlike feature

--- core/views.py
+++ core/views.py
@@ -31,13 +31,24 @@ def upload(request):
 
 @login_required(login_url='signin')
 def like_post(request):
-    username - request.user.username
+    username = request.user.username
     post_id = request.GET.get('post_id')
 
-    post = Posts.objects.get(id=post_id)
+    post = Post.objects.get(id=post_id)
 
     like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
     
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
 
 @login_required(login_url='signin')
 def settings(request):
