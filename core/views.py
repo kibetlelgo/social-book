85fce5d21edfd7ea5238ee34507044c13d05a7fd
kibet <kibetlelgo49@gmail.com>
2025-03-26 11:33:17 +0300

    post feed update, download post images

--- core/views.py
+++ core/views.py
@@ -4,14 +4,57 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
+import random
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_list = Post.objects.filter(user=usernames)
+        feed.append(feed_list)
+
+    feed_list = list(chain(*feed))
+
+    #user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestion_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestion_list = [x for x in list(new_suggestion_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestion_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestion_list:
+        username_profile.append(user.id)
+
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_list, 'suggestions_username_profile_list':suggestions_username_profile_list[:4]})
 
 @login_required(login_url='signin')
 def upload(request):
@@ -61,9 +104,9 @@ def profile(request, pk):
     user = pk
 
     if FollowersCount.objects.filter(follower=follower, user=user).first():
-        button_text = "Unfollow"
+        button_text = 'Unfollow'
     else:
-        button_text = "Follow"
+        button_text = 'Follow'
 
     user_followers = len(FollowersCount.objects.filter(user=pk))
     user_following = len(FollowersCount.objects.filter(follower=pk))
@@ -176,7 +219,6 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
-
 
 @login_required(login_url='signin')
 def logout(request):
